ad3a28d45fc5b240ce3ee2c3e08a9fb0a1a767b2
Юлия Алексина <ulchez@mail.ru>
2022-10-02 18:55:34 +0300

    Добавьте файлы проекта.

---
+++ Lab14WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab14WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Product> products;
+        public MainWindow()
+        {
+            InitializeComponent();
+            products = new ObservableCollection<Product>();
+            products.Add(new Product()
+            {
+                ProdName = "Бананы                ",
+                ProdPrice = 70,
+                ProdImage = "Data/Бананы.jpg",
+                ProductCategory = ProductCategorys.Food
+            });
+            products.Add(new Product()
+            {
+                ProdName = "Кокосы                ",
+                ProdPrice = 150,
+                ProdImage = "Data/Кокосы.jpg",
+                ProductCategory = ProductCategorys.Food
+            }); 
+            products.Add(new Product()
+            {
+                ProdName = "Ананасы             ",
+                ProdPrice = 230,
+                ProdImage = "Data/Ананасы.jpg",
+                ProductCategory = ProductCategorys.Food
+            });
+            products.Add(new Product()
+            {
+                ProdName = "Пылесосы",
+                ProdPrice = 7000,
+                ProdImage = "Data/Пылесосы.jpg",
+                ProductCategory = ProductCategorys.Appliances
+            });
+            products.Add(new Product()
+            {
+                ProdName = "Фены",
+                ProdPrice = 2500,
+                ProdImage = "Data/Фены.jpg",
+                ProductCategory = ProductCategorys.Appliances
+            });
+            products.Add(new Product()
+            {
+                ProdName = "Чайники",
+                ProdPrice = 1500,
+                ProdImage = "Data/Чайники.jpg",
+                ProductCategory = ProductCategorys.Appliances
+            });
+            listBox.ItemsSource = products;
+        }
+    }
+}
